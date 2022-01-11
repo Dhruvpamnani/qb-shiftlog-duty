380dabd9235ce45509288471bd25f11512214daa
Dhruv Pamnani <90241955+Dhruvpamnani@users.noreply.github.com>
2022-01-11 09:30:13 +0530

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,7 +1,7 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 ui_page "html/index.html"
-description "Qbus Attendence Made by LEGEND|TGB#1058"
+description "QBCORE Attendence
 server_scripts {
     'server/server.lua',
     'config.lua',
